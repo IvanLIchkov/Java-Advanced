ddc1344665ca5cd440aee4d275d5b655b7bf5c68
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-10 15:48:42 +0300

    Infix to postfix task.

---
+++ StackAndQueues_4_exe/InfixToPostfix.java
@@ -0,0 +1,10 @@
+package StackAndQueues_4_exe;
+
+import java.util.Scanner;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
