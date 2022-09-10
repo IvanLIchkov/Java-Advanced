d30b2ad752467eef0ebe95c2e19526b7b8feeac6
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-10 15:46:27 +0300

    Simple text editor task 100/100 solution in judge replace push from end of case to the beginning of the case.

--- StackAndQueues_4_exe/SimpleTextEditor.java
+++ StackAndQueues_4_exe/SimpleTextEditor.java
@@ -7,31 +7,31 @@ public class SimpleTextEditor {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numbersOfOperations=Integer.parseInt(scan.nextLine());
-        ArrayDeque<String> historyStack=new ArrayDeque<>();
+        ArrayDeque<String> historyQueue=new ArrayDeque<>();
         StringBuilder text=new StringBuilder();
         for (int i = 0; i <numbersOfOperations ; i++) {
             String[] commands=scan.nextLine().split("\\s+");
             switch (commands[0]){
                 case "1":
+                    historyQueue.push(String.valueOf(text));
                     text.append(commands[1]);
-                    historyStack.push(String.valueOf(text));
                     break;
                 case "2":
+                    historyQueue.push(String.valueOf(text));
                     int count=Integer.parseInt(commands[1]);
                     text.delete(text.length()-count,text.length());
-                    historyStack.push(String.valueOf(text));
                     break;
                 case "3":
                     System.out.println(text.charAt(Integer.parseInt(commands[1])-1));
                     break;
                 case "4":
-                    if (historyStack.isEmpty()){
-                        break;
-                    }
-                    historyStack.poll();
-                    text= new StringBuilder(historyStack.peek());
+//                    if (historyQueue.isEmpty()){
+//                        break;
+//                    }
+                    text= new StringBuilder(historyQueue.pop());
                     break;
             }
         }
     }
 }
+
