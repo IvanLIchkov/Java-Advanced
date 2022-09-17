b869e150687261d9926af8f83ccbd8aad7b35fb4
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-17 23:32:03 +0300

    Intersection of two matrices task.

---
+++ MultidimensionalArrays_5/IntersectionOfTwoMatrices.java
@@ -0,0 +1,10 @@
+package MultidimensionalArrays_5;
+
+import java.util.Scanner;
+
+public class IntersectionOfTwoMatrices {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
