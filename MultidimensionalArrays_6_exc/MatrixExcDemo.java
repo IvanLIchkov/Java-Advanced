a3673e0431af2fe34f19b293271d2f8f1cc950ce
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-18 17:17:08 +0300

    Exercise of matrices.

---
+++ MultidimensionalArrays_6_exc/MatrixExcDemo.java
@@ -0,0 +1,10 @@
+package MultidimensionalArrays_6_exc;
+
+import java.util.Scanner;
+
+public class MatrixExcDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
