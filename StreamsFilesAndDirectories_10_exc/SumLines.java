784f6a126b8a977aba6d39aedd7e09e7e2fec02d
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-26 14:06:09 +0300

    Sum lines task solution.

--- StreamsFilesAndDirectories_10_exc/SumLines.java
+++ StreamsFilesAndDirectories_10_exc/SumLines.java
@@ -1,10 +1,29 @@
 package StreamsFilesAndDirectories_10_exc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class SumLines {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String input="/Users/scopi/Desktop/Java-Advanced/StreamsFilesAndDirectories_10_exc/input.txt";
+        try(BufferedReader reader= Files.newBufferedReader(Paths.get(input))) {
+            String line = reader.readLine();
+            while (line!=null){
+                long sum=0;
+                for (char c : line.toCharArray()) {
+                    sum+=c;
+                }
+                System.out.println(sum);
+                line=reader.readLine();
 
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
