2abb156f9f46e37219adf6fb27e5f4e65d98c119
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-27 17:24:49 +0300

    Sort numbers with stream API function.

---
+++ FunctionalProgramming_11/SumNumbers.java
@@ -0,0 +1,10 @@
+package FunctionalProgramming_11;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
