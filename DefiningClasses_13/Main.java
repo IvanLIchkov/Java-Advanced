eda3bbc53796eb41cc1e4c36aafeb909d749bfd3
Scopi1 <Vanko.lichkov@abv.bg>
2022-09-30 15:41:22 +0300

    Constructor task solution and files.

--- DefiningClasses_13/Main.java
+++ DefiningClasses_13/Main.java
@@ -8,17 +8,17 @@ public class Main {
         int carsCount = Integer.parseInt(scan.nextLine());
         while (carsCount-- > 0) {
             String[] tokens = scan.nextLine().split("\\s+");
-            String brand = tokens[0];
-            String model = tokens[1];
-            int horsePower = Integer.parseInt(tokens[2]);
-            Car car = new Car();
-            car.setBrand(brand);
-            car.setModel(model);
-            car.setHorsePower(horsePower);
+            String brand=tokens[0];
 
+            Car car;
+            if (tokens.length==1){
+                car=new Car(brand);
+            }else{
+                String model = tokens[1];
+                int horsePower = Integer.parseInt(tokens[2]);
+                car=new Car(brand, model, horsePower);
+            }
             System.out.println(car.carInfo());
-
         }
-
     }
 }
