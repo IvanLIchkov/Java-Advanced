23f037cd9e3d13add2478f70ad148f2303b98249
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-04 12:34:34 +0300

    Generics.

---
+++ Generics_17/Jar/Main.java
@@ -0,0 +1,10 @@
+package Generics_17.Jar;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
