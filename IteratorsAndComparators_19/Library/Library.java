d92217e269a7fcabc31fe1cf49e893cc62672e11
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-06 11:43:05 +0300

    Library task solution.

---
+++ IteratorsAndComparators_19/Library/Library.java
@@ -0,0 +1,30 @@
+package IteratorsAndComparators_19.Library;
+
+import IteratorsAndComparators_19.Book.Book;
+
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index<books.length;
+            }
+
+            @Override
+            public Book next() {
+                return books[index++];
+            }
+        };
+    }
+}
