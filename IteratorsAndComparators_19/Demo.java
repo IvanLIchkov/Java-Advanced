03ebc1bd0b42941b276fb852c506bef3fb41f490
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-06 09:51:26 +0300

    Параметър с вариращ брой на аргументите.

--- IteratorsAndComparators_19/Demo.java
+++ IteratorsAndComparators_19/Demo.java
@@ -1,9 +1,20 @@
 package IteratorsAndComparators_19;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        printString("Hello", "world", "!");
+        printString("Hello", "world");
+        printString();
+    }
+    public static void printString(String... str){
+        String[] strings=str;
+        if (strings.length==0){
+            System.out.println("Please add input");
+        }
+        Arrays.stream(strings).forEach(System.out::println);
     }
 }
