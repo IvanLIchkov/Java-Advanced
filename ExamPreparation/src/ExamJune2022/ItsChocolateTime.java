0037c5cf2f3368eb76e889f54405857e07dce6ac
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-19 09:57:26 +0300

    Exam June 2022 first task.

---
+++ ExamPreparation/src/ExamJune2022/ItsChocolateTime.java
@@ -0,0 +1,9 @@
+package ExamJune2022;
+
+import java.util.Scanner;
+
+public class ItsChocolateTime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
